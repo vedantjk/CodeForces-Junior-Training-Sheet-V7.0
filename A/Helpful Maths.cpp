8d62997df37106a132ea6668f01600ed9444228c
vedantjk <69514666+vedantjk@users.noreply.github.com>
2022-05-02 22:32:37 +0530

    Create Helpful Maths.cpp

---
+++ A/Helpful Maths.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin>>s;
+    int n=s.size();
+    string s2;
+    
+    for(int i=0;i<n;i+=2){
+    s2+=s[i];
+    }
+    
+    int m = s2.size();
+    sort(s2.begin(),s2.end());
+    
+    for(int i=0;i<m;i++){
+        if(i==m-1)
+        cout<<s2[i];
+        else
+        cout<<s2[i]<<"+";
+    }
+    
+    return 
