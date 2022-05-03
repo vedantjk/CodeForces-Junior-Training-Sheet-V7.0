092553933d98dfb2d3133cd599d4ce872cf32500
vedantjk <69514666+vedantjk@users.noreply.github.com>
2022-05-03 07:30:54 +0530

    Create Brain's Photos.cpp

---
+++ A/Brain's Photos.cpp
@@ -0,0 +1,17 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+    int main() {
+    int n,m;
+    cin>>n>>m;
+    char c;
+    for(int i=0;i<n*m;i++){
+    cin>>c;
+    if(c=='C' || c=='M' || c=='Y')
+    {
+    cout<<"#Color";
+    return 0;
+    }
+    }
+    cout<<"#Black&White";
+    return 0;
+    }
